5ef236dc66e04eeb83b2e99f3370d54fd18dc471
git <sunjf@biomarker.com.cn>
2017-05-03 09:29:28 +0800

    test

--- src/main/java/thread/waitandnotify/pc/Factory.java
+++ src/main/java/thread/waitandnotify/pc/Factory.java
@@ -6,30 +6,34 @@ public class Factory {
 
     public void pro() {
         synchronized (object) {
-            if(count > 0) {
+            if (count > 0) {
                 try {
+                    System.out.println("pro wait | " + Thread.currentThread().getName());
                     object.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
             count++;
-            System.out.println("pro : " + count);
+            System.out.println("pro : " + count + " | " + Thread.currentThread().getName());
             object.notify();
         }
     }
 
     public void cus() {
         synchronized (object) {
-            if(count <= 0) {
+            if (count <= 0) {
                 try {
+                    System.out.println("cus wait | " + Thread.currentThread().getName());
                     object.wait();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            count--;
-            System.out.println("cus : " + count);
+            if(count > 0) {
+                count--;
+                System.out.println("cus : " + count + " | " + Thread.currentThread().getName());
+            }
             object.notify();
         }
     }
--- src/main/java/thread/waitandnotify/pc/PCModel.java
+++ src/main/java/thread/waitandnotify/pc/PCModel.java
@@ -7,8 +7,13 @@ public class PCModel {
     public static void main(String[] args) {
         Factory f = new Factory();
         ProducerThread pt = new ProducerThread(f);
-        CustomerThread ct = new CustomerThread(f);
+        CustomerThread ct1 = new CustomerThread(f);
+        CustomerThread ct2 = new CustomerThread(f);
         pt.start();
-        ct.start();
+        pt.setName("pt");
+        ct1.start();
+        ct1.setName("ct1");
+        ct2.start();
+        ct2.setName("ct2");
     }
 }
