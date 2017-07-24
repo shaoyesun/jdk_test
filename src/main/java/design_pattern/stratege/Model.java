3dfa953d1d7961e3d9c963a4a2b1f9cd14afb0ff
sunjf <sunjf@biomarker.com.cn>
2017-07-24 14:25:25 +0800

    stratege

--- src/main/java/design_pattern/stratege/Model.java
+++ src/main/java/design_pattern/stratege/Model.java
@@ -1,7 +1,7 @@
 package design_pattern.stratege;
 
 /**
- *   @策略模式中有4个对象
+ *   @适用于同一操作的不同行为,策略模式中有4个对象
  *   @ 1.环境对象(Context)：该类中实现了对抽象策略中定义的接口或者抽象类的引用。Duck
  *   @ 2.抽象策略对象(Strategy)：它可由接口或抽象类来实现。SwimmingStratege
  *   @ 3.具体策略对象(ConcreteStrategy)：它封装了实现同不功能的不同算法 SwimmingWithFoot
