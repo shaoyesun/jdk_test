e9c1fb42feb0c0aecd6e77bb8bdc2f71c5a13eb1
sunjf <sunjf@biomarker.com.cn>
2017-07-27 22:26:57 +0800

    死锁

--- src/main/java/thread/DeadLock/DeadLock.java
+++ src/main/java/thread/DeadLock/DeadLock.java
@@ -5,41 +5,28 @@ package thread.DeadLock;
  */
 public class DeadLock implements Runnable {
     public String name;
-    public Object object1 = new Object();
-    public Object object2 = new Object();
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    public Object object1 = new Object(),object2 = new Object();
     public void run() {
         if(name.equals("a")) {
             synchronized (object1) {
-                System.out.println(name + " 获得 object1");
-                System.out.println(name + " 等待 object2");
+                System.out.println(name + " 获得 object1，等待 object2");
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object2) {
-                    System.out.println("1-->2");
-                }
+                } catch (InterruptedException e) { e.printStackTrace();}
+                synchronized (object2) { System.out.println("1-->2");}
             }
         }
         if(name.equals("b")) {
             synchronized (object2) {
-                System.out.println(name + " 获得 object2");
-                System.out.println(name + " 等待 object1");
+                System.out.println(name + " 获得 object2,等待 object1");
                 try {
                     Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object1) {
-                    System.out.println("2-->1");
-                }
+                } catch (InterruptedException e) { e.printStackTrace();}
+                synchronized (object1) { System.out.println("2-->1");}
             }
         }
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
