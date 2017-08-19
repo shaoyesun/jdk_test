414d1b0a773f39765ecec0e58e32e6695695b83c
sunjf <sunjf@biomarker.com.cn>
2017-08-19 20:56:54 +0800

    string测试

---
+++ src/main/java/string/StringHelper.java
@@ -0,0 +1,39 @@
+package string;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 打印字符串的所有排列
+ */
+public class StringHelper {
+    public static Set<String> permutationFinder(String str) {
+        Set<String> perm = new HashSet<String>();
+        if (str == null) {
+            return null;
+        } else if (str.length() == 0) {
+            perm.add("");
+            return perm;
+        }
+        char initial = str.charAt(0); // first character
+        String rem = str.substring(1); // Full string without first character
+        Set<String> words = permutationFinder(rem);
+        for (String strNew : words) {
+            for (int i = 0;i<=strNew.length();i++){
+                perm.add(charInsert(strNew, initial, i));
+            }
+        }
+        return perm;
+    }
+
+    public static String charInsert(String str, char c, int j) {
+        String begin = str.substring(0, j);
+        String end = str.substring(j);
+        return begin + c + end;
+    }
+
+    public static void main(String[] args) {
+        String s = "AABC";
+        System.out.println("Permutations for " + s + " are: " + permutationFinder(s));
+    }
+}
--- src/main/java/string/StringTest.java
+++ src/main/java/string/StringTest.java
@@ -1,19 +1,53 @@
 package string;
 
-import java.util.Arrays;
+import org.apache.tools.ant.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by root on 17-8-1.
  */
 public class StringTest {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("stringbuffer");
-        String s = "string";
+        //统计字符出现次数
+        String str6 = "abcd统计字符出现次数abcd统计字符出现次数";
+        Map<String, Integer> map = new HashMap();
+        for(int i = 0; i < str6.length(); i++) {
+            String key = String.valueOf(str6.charAt(i));
+            if(map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        System.out.println("5、统计字符出现次数：" + map);
 
-        int[] a = {1,2,3};
-        int[] b = {4,5,6};
-        int[] c = Arrays.copyOf(a, 6);
-        System.out.println(c);
+        //比较两个字符串
+        String str4 = "abcd";
+        String str5 = "bcde";
+        System.out.println("4、比较两个字符串：" + str4.compareTo(str5));
+
+        //字符串大小写转换
+        String str3 = "abcd";
+        System.out.println("3、字符串大小写转换：" + str3.toUpperCase());
+
+        //判断字符串是否对称
+        String str2 = "abcdeedcba";
+        String index = new StringBuffer(str2).reverse().toString();
+        System.out.println("2、判断字符串是否对称，利用StringBuffer的reverse：" + str2.equals(index));
+        boolean b = true;
+        for(int i = 0; i < str2.length()/2; i++) {
+            //System.out.println(str2.charAt(i));
+            if(str2.charAt(i) != str2.charAt(str2.length() -1 - i)) {
+                b = false;
+                break;
+            }
+        }
+        System.out.println("2、判断字符串是否对称，字符串两边查找对比：" + b);
+
+        //删除字符串中制定字符
+        String str1 = "abcdabcdabcd";
+        System.out.println("1、字符串中删除a字符：" + str1.replace(Character.toString('a'), ""));
     }
 }
