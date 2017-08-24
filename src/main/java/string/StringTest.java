6b93a90a4b85d609babe2043c33ecf82efc2c8bd
sunjf <sunjf@biomarker.com.cn>
2017-08-24 22:19:56 +0800

    string测试

--- src/main/java/string/StringTest.java
+++ src/main/java/string/StringTest.java
@@ -1,7 +1,5 @@
 package string;
 
-import org.apache.tools.ant.util.StringUtils;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,6 +8,30 @@ import java.util.Map;
  */
 public class StringTest {
     public static void main(String[] args) {
+
+    }
+
+    //字符串hash值及字符串对象手动放入常量池学习
+    public static void hashAndIntern() {
+        String s1 = new String("abc");
+        String s2 = new String("abc");
+        System.out.println("s1 = " + s1.hashCode());//拥有相同值的字符串，hash值相等，因为hash值是通过值计算的
+        System.out.println("s2 = " + s2.hashCode());
+        System.out.println(s1 == s2);
+
+        String s3 = "abc";//此时的字符串为常量，在内存（常量池）中独一份
+        String s4 = "abc";
+        System.out.println(s3 == s4);//true
+
+        //intern()方法，将字符串对象从堆中放入常量池中
+        s1 = s1.intern();//如果此时常量池中存在该字符串则返回引用，否则在常量池中创建该值返回引用
+        s2 = s2.intern();
+        System.out.println(s1 == s2);//true
+        System.out.println(s1 == s3);//true
+    }
+
+    //常见面试题
+    public static void interview() {
         //统计字符出现次数
         String str6 = "abcd统计字符出现次数abcd统计字符出现次数";
         Map<String, Integer> map = new HashMap();
@@ -50,4 +72,5 @@ public class StringTest {
         String str1 = "abcdabcdabcd";
         System.out.println("1、字符串中删除a字符：" + str1.replace(Character.toString('a'), ""));
     }
+
 }
